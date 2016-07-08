f0ebed51b51f869429b1309aeddf51a602462537
blake21 <blaketaylor76@gmail.com>
2016-07-08 17:41:42 -0600

    Create json.js

---
+++ json.js
@@ -0,0 +1,16 @@
+jsonParse([
+{
+   "first": "Blake", "last":"Taylor", "gpa":"3.8", "major":"Software Engineering"
+},
+{
+   "first": "Conrad", "last":"Taylor", "gpa":"2.0", "major":"Electrical Engineering"
+},
+{
+   "first":"Taylor", "last":"Childs", "gpa":"2.01", "major":"Automotive Technician"
+},
+{
+   "first":"Mickey", "last":"Mouse", "gpa":"3.5", "major":"Entertainment"
+},
+{
+   "first":"Snoop", "last":"Dogg", "gpa":"4.0", "major":"Thug"
+}]);
